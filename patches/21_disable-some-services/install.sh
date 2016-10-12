2723fe1660b6e72179da1d29c18ac5b7c376d222
maz-1 <ohmygod19993@gmail.com>
2016-10-12 19:45:36 +0800

    update

--- patches/21_disable-some-services/install.sh
+++ patches/21_disable-some-services/install.sh
@@ -5,3 +5,4 @@ cd $basedir
 
 patch -p1 < $patchdir/disable_p2pblock_by_default.patch
 patch -p1 < $patchdir/disable_privoxy_by_default.patch
+sed -i "s/option adb_enabled .*/option adb_enabled '0'/g" $basedir/feeds/packages/net/adblock/files/adblock.conf
--- patches/69_luci_move_category/install.sh
+++ patches/69_luci_move_category/install.sh
@@ -10,5 +10,8 @@ patch --no-backup-if-mismatch -p1 < $patchdir/luci_move_category.patch
 sed -i 's/\"services\"/\"nas\"/g' $basedir/feeds/luci/applications/luci-app-minidlna/luasrc/controller/minidlna.lua
 sed -i 's;admin/services;admin/nas;g' $basedir/feeds/luci/applications/luci-app-minidlna/luasrc/view/minidlna_status.htm
 
+sed -i 's/"services"/"network"/g' $basedir/feeds/luci/applications/luci-app-adblock/luasrc/controller/adblock.lua
+
 #patch -p1 < $patchdir/openconnect-disable-gssapi.patch
+#sed -i 's/\tend$/\tend\n\n\tentry({"admin", "nas"}, firstchild(), "NAS", 45).dependent = false/g' $basedir/
 
