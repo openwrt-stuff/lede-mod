27c46c4adc4e88bd5a08cb57a8cbbef2166b81f2
maz-1 <ohmygod19993@gmail.com>
2016-10-29 19:17:14 +0800

    update

--- patches/22_ip-full-dependency-fix/install.sh
+++ patches/22_ip-full-dependency-fix/install.sh
@@ -5,5 +5,6 @@ basedir=$(cd `dirname $0`;cd ../../../;pwd)
 cd $basedir
 patch -p1 < $patchdir/ip-full-dependency-fix_provides.patch
 patch -p1 < $patchdir/ip-full-dependency-fix-mwan3.patch
+patch -p1 < $patchdir/disable_mwan.patch
 #rm -rf package/feeds/packages/mwan3
 #rm -rf package/feeds/packages/mwan3-luci
