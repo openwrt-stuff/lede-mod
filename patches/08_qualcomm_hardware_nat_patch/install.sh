74bd1a7d5ac6a98aff5e41c5cdc116714ada5726
maz-1 <ohmygod19993@gmail.com>
2017-08-07 15:19:58 +0000

    dont install

--- patches/08_qualcomm_hardware_nat_patch/install.sh
+++ patches/08_qualcomm_hardware_nat_patch/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 patchdir=$(cd `dirname $0`;pwd)
 basedir=$(cd `dirname $0`;cd ../../../;pwd)
-
+exit 0
 ls $patchdir | grep -P "^\d\.\d+$" | while read KVER
 do
     cp $patchdir/${KVER}/* \
